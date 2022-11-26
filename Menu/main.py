338ab902ae38638055be806e2c146f72d4171f70
Luk4s30 <72667606+Luk4s30@users.noreply.github.com>
2022-11-26 22:18:43 +0100

    Add files via upload

--- Menu/main.py
+++ Menu/main.py
@@ -1,7 +1,9 @@
 import pygame
+from pygame import mixer
 import przycisk
 
 pygame.init()
+mixer.init()
 
 #wielkość okna
 SCREEN_WIDTH = 1920
@@ -13,10 +15,14 @@ pygame.display.set_caption("Gra")
 game_paused = True
 status_menu = "menu"
 status_car = 1
+muzyka = 1
 
 font = pygame.font.SysFont("arialblack", 40)    #czcionka
+font2 = pygame.font.SysFont("arialblack", 120)
 
 TEXT_COL = (255, 255, 255)  #kolor
+TEXT_COL2 = (0, 0, 0)  #kolor
+
 
 #ładuje zdjęcia
 resume_img = pygame.image.load("images/button_resume.png").convert_alpha()
@@ -35,38 +41,50 @@ car2_img = pygame.image.load('images/car/car2.png').convert_alpha()
 car3_img = pygame.image.load('images/car/car3.png').convert_alpha()
 car4_img = pygame.image.load('images/car/car4.png').convert_alpha()
 
-#tworzy przycisk
-graj_przy = przycisk.Przycisk(710, 100, resume_img, 1)
-ustawienia_przy = przycisk.Przycisk(760, 320, options_img, 1)
-wyjscie_przy = przycisk.Przycisk(810, 660, quit_img, 1)
-grafika_przy = przycisk.Przycisk(226, 75, video_img, 1)
-dzwienk_przy = przycisk.Przycisk(225,200, audio_img, 1)
-klawisze_przy = przycisk.Przycisk(246,325, keys_img, 1)
-powrot_przy = przycisk.Przycisk(810,660, back_img, 1)
-pojazdy_przy = przycisk.Przycisk(760, 490, pojazdy_img, 1)
-strz_l_przy = przycisk.Przycisk(1700, 500,strzalka_l_img, 1)
-strz_p_przy = przycisk.Przycisk(100, 500,strzalka_p_img, 1)
+tlo_img = pygame.image.load('images/tlo.png').convert_alpha()
 
-car1_przy = przycisk.Przycisk(730, 100,car1_img, 0.8)
-car2_przy = przycisk.Przycisk(730, 100,car2_img, 0.8)
-car3_przy = przycisk.Przycisk(730, 100,car3_img, 0.8)
-car4_przy = przycisk.Przycisk(730, 100,car4_img, 0.8)
+#tworzy przycisk
+graj_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 300, resume_img, 1)
+ustawienia_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 500, options_img, 1)
+wyjscie_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 830, quit_img, 1)
+grafika_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 75, video_img, 1)
+dzwienk_przy = przycisk.Przycisk(SCREEN_WIDTH/2,200, audio_img, 1)
+klawisze_przy = przycisk.Przycisk(SCREEN_WIDTH/2,325, keys_img, 1)
+powrot_przy = przycisk.Przycisk(SCREEN_WIDTH/2,700, back_img, 1)
+powrot2_przy = przycisk.Przycisk(SCREEN_WIDTH/2,900, back_img, 1)
+pojazdy_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 670, pojazdy_img, 1)
+
+strz_l_przy = przycisk.Przycisk(SCREEN_WIDTH/2+300, 500,strzalka_l_img, 1)
+strz_p_przy = przycisk.Przycisk(SCREEN_WIDTH/2-300, 500,strzalka_p_img, 1)
+
+car1_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 500,car1_img, 0.8)
+car2_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 500,car2_img, 0.8)
+car3_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 500,car3_img, 0.8)
+car4_przy = przycisk.Przycisk(SCREEN_WIDTH/2, 500,car4_img, 0.8)
 
 def rysuj_tekst(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
-  screen.blit(img, (x, y))
+  text = img.get_rect(center=(SCREEN_WIDTH/2+x, y))
+  screen.blit(img, text)
+
+mixer.init()
+mixer.music.load('muzyka/Music_bg.wav')
+mixer.music.play(-1)
 
 #pentla gry
 run = True
+print('1')
 while run:
-
-  screen.fill((52, 78, 91))
+    
+  screen.blit(tlo_img, (0, 0))
 
   #gra sprawdza czy jest włączony tryb gry
   if game_paused == True:
     #sprawdza status menu
     if status_menu == "menu":
       #rysuje przyciski
+      rysuj_tekst("CAR DRIVE", font2, TEXT_COL2, 5, 105)
+      rysuj_tekst("CAR DRIVE", font2, TEXT_COL, 0, 100)
       if graj_przy.rysuj(screen):
         pygame.time.wait(200)
         game_paused = False
@@ -86,6 +104,7 @@ while run:
         print("Video Settings")
       if dzwienk_przy.rysuj(screen):
         pygame.time.wait(200)
+        mixer.music.stop()
         print("Audio Settings")
       if klawisze_przy.rysuj(screen):
         pygame.time.wait(200)
@@ -102,7 +121,7 @@ while run:
         if strz_p_przy.rysuj(screen):
           status_car = 2
           pygame.time.wait(200)
-        if powrot_przy.rysuj(screen):
+        if powrot2_przy.rysuj(screen):
           pygame.time.wait(200)
           status_menu = "menu"
         if car1_przy.rysuj(screen):
@@ -115,7 +134,7 @@ while run:
         if strz_p_przy.rysuj(screen):
           status_car = 3
           pygame.time.wait(200)
-        if powrot_przy.rysuj(screen):
+        if powrot2_przy.rysuj(screen):
           pygame.time.wait(200)
           status_menu = "menu"
         if car2_przy.rysuj(screen):
@@ -128,7 +147,7 @@ while run:
         if strz_p_przy.rysuj(screen):
           status_car = 4
           pygame.time.wait(200)
-        if powrot_przy.rysuj(screen):
+        if powrot2_przy.rysuj(screen):
           pygame.time.wait(200)
           status_menu = "menu"
         if car3_przy.rysuj(screen):
@@ -141,7 +160,7 @@ while run:
         if strz_p_przy.rysuj(screen):
           status_car = 1
           pygame.time.wait(200)
-        if powrot_przy.rysuj(screen):
+        if powrot2_przy.rysuj(screen):
           pygame.time.wait(200)
           status_menu = "menu"
         if car4_przy.rysuj(screen):
@@ -167,5 +186,4 @@ while run:
       run = False
 
   pygame.display.update()
-
 pygame.quit()
